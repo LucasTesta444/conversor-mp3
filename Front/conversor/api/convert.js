b5bbd5412a8c781ff0fd720c455be311788e8294
Lucas Testa <lucastesta27@icloud.com>
2025-03-24 22:13:57 -0300

    atualizacoes de melhorias

---
+++ Front/conversor/api/convert.js
@@ -0,0 +1,53 @@
+// Front/conversor/api/convert.js
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+  const { url, format } = req.body;
+
+  if (!url || !format) {
+    return res.status(400).json({ error: 'URL e formato são obrigatórios!' });
+  }
+
+  const videoId = extractVideoId(url);
+
+  if (!videoId) {
+    return res.status(400).json({ error: 'URL inválida!' });
+  }
+
+  const apiKey = process.env.API_KEY;
+  const apiHost = process.env.API_HOST;
+
+  const options = {
+    method: 'GET',
+    url: `https://${apiHost}/dl`,
+    params: { id: videoId },
+    headers: {
+      'x-rapidapi-key': apiKey,
+      'x-rapidapi-host': apiHost,
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    const downloadUrl = response.data.link;
+
+    if (downloadUrl) {
+      res.status(200).json({ download_url: downloadUrl }); // Use 200 para sucesso
+    } else {
+      res.status(500).json({ error: 'Erro ao obter o link de download.' });
+    }
+  } catch (error) {
+    console.error('Erro na API:', error);
+    res.status(500).json({ error: 'Erro ao processar a conversão. Tente novamente.' });
+  }
+}
+
+function extractVideoId(url) {
+  const regex = /(?:https?:\/\/(?:www\.)?youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=))([a-zA-Z0-9_-]{11})/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
+}
